8c394f4fea4596fd9cabace463a606f726e30ac2
Matheus <matheus.resteves@gmail.com>
2024-09-16 20:05:34 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using minimal_api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if (loginDTO.Email == "teste@gmail.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login efetuado com sucesso!");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
