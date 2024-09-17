8d45087d1f91de40a6bef8a8d18140b3ad0f88cc
matheus <matheus.resteves@gmail.com>
2024-09-17 03:27:46 -0300

    Adicionando um administrador

--- Infrastructure/Db/DbContexto.cs
+++ Infrastructure/Db/DbContexto.cs
@@ -12,6 +12,18 @@ namespace minimal_api.Infrastructure.Db
         }
         public DbSet<Administrator> Administrators { get; set; } = default!;
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrator>().HasData(
+                new Administrator {
+                    Id = 1,
+                    Email = "teste@gmail.com",
+                    Password = "123456",
+                    Profile = "Adm"
+                }
+            );
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
